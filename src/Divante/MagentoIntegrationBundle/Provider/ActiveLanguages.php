9a4008a0b9fe59c6deafb4295efecc348bcb41d6
Michal Bolka <mbolka@divante.pl>
2019-09-16 14:21:19 +0200

    Add active languages provider

---
+++ src/Divante/MagentoIntegrationBundle/Provider/ActiveLanguages.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * @category    bosch-stuttgart
+ * @date        16/09/2019
+ * @author      Michał Bolka <mbolka@divante.co>
+ * @copyright   Copyright (c) 2019 Divante Ltd. (https://divante.co)
+ */
+
+namespace Divante\MagentoIntegrationBundle\Provider;
+
+use Pimcore\Model\DataObject\ClassDefinition\Data;
+use Pimcore\Model\DataObject\ClassDefinition\DynamicOptionsProvider\SelectOptionsProviderInterface;
+use Pimcore\Model\DataObject\Classificationstore\StoreConfig;
+use Pimcore\Tool;
+
+/**
+ * Class ActiveLanguages
+ * @package Divante\MagentoIntegrationBundle\Provider
+ */
+class ActiveLanguages implements SelectOptionsProviderInterface
+{
+    /**
+     * @param array $context
+     * @param Data $fieldDefinition
+     *
+     * @return array
+     * @throws \Exception
+     */
+    public function getOptions($context, $fieldDefinition): array
+    {
+        return Tool::getValidLanguages();
+    }
+
+    /**
+     * @param array $context
+     * @param Data $fieldDefinition
+     *
+     * @return bool
+     */
+    public function hasStaticOptions($context, $fieldDefinition): bool
+    {
+        return false;
+    }
+
+    /**
+     * @param array $context
+     * @param Data $fieldDefinition
+     *
+     * @return mixed
+     */
+    public function getDefaultValue($context, $fieldDefinition)
+    {
+        return 'en';
+    }
+}
