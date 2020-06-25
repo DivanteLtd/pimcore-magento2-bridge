12dfaba269f662370b523e4aa70785a6e4b23e1e
thibault <dehacq.thibault@gmail.com>
2020-06-25 09:31:04 +0200

    Endpoint to get Assets

--- src/Divante/MagentoIntegrationBundle/Application/Asset/MappedAssetService.php
+++ src/Divante/MagentoIntegrationBundle/Application/Asset/MappedAssetService.php
@@ -4,6 +4,7 @@ namespace Divante\MagentoIntegrationBundle\Application\Asset;
 
 use Pimcore\Model\Asset;
 use Pimcore\Model\Webservice\Data\Mapper;
+use Symfony\Component\Routing\RouterInterface;
 
 /**
  * Class MappedAssetService
@@ -11,6 +12,21 @@ use Pimcore\Model\Webservice\Data\Mapper;
  */
 class MappedAssetService
 {
+
+    /**
+     * @var RouterInterface
+     */
+    protected $router;
+
+    /**
+     * MappedAssetService constructor.
+     * @param RouterInterface $router
+     */
+    public function __construct(RouterInterface $router)
+    {
+        $this->router = $router;
+    }
+
     /**
      * @param string $id
      * @param string|null $thumbnail
@@ -27,16 +43,34 @@ class MappedAssetService
             ];
         }
 
+        $algo = 'sha1';
         $outputAsset = Mapper::map($asset, "Pimcore\Model\Webservice\Data\Asset\File\Out", 'out');
         if ($thumbnail && $asset instanceof Asset\Image) {
             $thumbnail = $asset->getThumbnail($thumbnail, false);
-            $thumbnailData = file_get_contents("http://localhost" . (string) $thumbnail);
+            $thumbnailData = file_get_contents(
+                sprintf(
+                    "%s://%s%s",
+                    $this->router->getContext()->getScheme(),
+                    $this->router->getContext()->getHost(),
+                    (string) $thumbnail
+                ));
 
             $thumbnailOutput = [];
             $thumbnailOutput['data'] = base64_encode($thumbnailData);
             $thumbnailOutput['mimetype'] = $thumbnail->getMimeType();
+            $checksum = hash($algo, $thumbnailData);
+            $thumbnailOutput["checksum"] = [
+                'algo' => $algo,
+                'value' => $checksum
+            ];
             $outputAsset->{"thumbnail"} = $thumbnailOutput;
         }
+
+
+        $outputAsset->checksum = [
+            'algo' => $algo,
+            'value' => $asset->getChecksum($algo)
+        ];
 
         return ['data' => $outputAsset];
     }
